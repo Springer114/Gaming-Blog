d819468388808d6519b67a543e46262dd7696527
Springer114 <springerzack04@yahoo.com>
2021-06-27 11:50:22 -0700

    added user model

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
     
-mongoose.connect('mongodb://localhost/insertdbnamehere', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/dftc_db', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("DB connection established"))
     .catch(err => console.log("Error connecting to DB", err));
