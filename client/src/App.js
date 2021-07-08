4355797f0735e82d6da6029ccf85d0df3992f6d4
Becky Hirsch <beckyhirsch@Beckys-MacBook-Air.local>
2021-07-07 18:44:29 -0700

    nav-and-default-styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './views/Home';
-import About from './views/About';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './index.css';
+import React from "react";
+import Header from "./components/Header.js";
+import "./index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTree } from "@fortawesome/free-solid-svg-icons";
+
+library.add( faTree );
 
 const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav className="navbar">
-          <ul className="nav">
-          <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-          </ul>
-          </nav>
-          <hr />
-          <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-          </Switch>
+    return (
+        <div className="App">
+            <Header />
         </div>
-      </Router>
-    </div>
-  );
-}
+    );
+};
 
 export default App;
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home page-wrapper">
-        <h1>Don't Forget the Chips!</h1>
-        <h2>A blog platform for gamers</h2>
+        <h1>A blog platform for gamers</h1>
+        <p>Welcome to our little corner of the web where we invite you to create a page and conversate about your gaming life.</p>
     </div>
   );
 }
