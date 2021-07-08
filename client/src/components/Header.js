7fa38937dc46ef3767d975468f4134aca808ab98
Springer114 <springerzack04@yahoo.com>
2021-07-07 20:03:05 -0700

    added login and registration views

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Home from "../views/Home";
 import About from "../views/About";
-
+import LoginForm from './LoginForm'
+import RegisterForm from "./RegisterForm";
 
 export default class Header extends Component {
     render() {
@@ -15,12 +16,16 @@ export default class Header extends Component {
                     <ul className="nav">
                         <li><Link to={"/"} className="nav-link">Home</Link></li>
                         <li><Link to={"/about"} className="nav-link">About</Link></li>
+                        <li><Link to={"/login"} className="nav-link">Login</Link></li>
+                        <li><Link to={"/register"} className="nav-link">Register</Link></li>
                     </ul>
                 </nav>
                 <hr />
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
+                    <Route path="/login" component={LoginForm} />
+                    <Route path="/register" component={RegisterForm} />
                 </Switch>
             </Router>
         );
