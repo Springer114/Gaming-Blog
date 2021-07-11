c29aa4700b57f6e2c66b117d72fa5d3864a3451f
Becky Hirsch <beckyhirsch@Beckys-MacBook-Air.local>
2021-07-11 13:29:31 -0700

    nav-styles-and-functionality

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Profile from "./views/UserProfile";
 
 function App() {
     return (
-        <div>
+        <div className="App">
             <Header />
             <Router>
                 <Home path="/" />
